b7760da08f68793c5a01211e6805466d4f23888f
C-Arisa <167191042+C-Arisa@users.noreply.github.com>
2024-04-27 03:30:43 +0000

    Finish static pages 03 chapter

--- test/controllers/hello_codespaces_controller_test.rb
+++ test/controllers/hello_codespaces_controller_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class HelloCodespacesControllerTest < ActionDispatch::IntegrationTest
-  # test "should get index" do
+  #test "should get index" do
   #   get "/"
   #   assert_response :success
   # end
