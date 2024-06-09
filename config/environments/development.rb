2059acc48c7af4da3a7b2d97b02b5b8ce4aefe8a
C-Arisa <167191042+C-Arisa@users.noreply.github.com>
2024-06-09 07:46:01 +0000

    pullreq chapter11

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,6 +42,13 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  host = 'urban-waddle-q7v5w6q77j4jf976j-3000.app.github.dev' 
+  # ここをコピペすると失敗します。
+  # 自分の環境のホストに変えてください。
+  # クラウドIDEの場合は以下をお使いください
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+
+
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
